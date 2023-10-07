b4e982c5016f3dea2ed1eba6cfcfae29edd4f68a
Isaac <govegeku00@gmail.com>
2023-10-07 18:43:54 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const wordArray =word.split("");
+  const reverseWordArray = wordArray.reverse();
+  const reverseWord = reverseWordArray.join("");
+  if(reverseWord === word){
+    return true;
+  }
+  return false;
 }
 
 /* 
-  Add your pseudocode here
-*/
+//   Add your pseudocode here
+// */
+// In the algorithm we first use the .split method in order to separate each letter of the strings to be tested
+// The next thing is to use the reverse method in order to rearrange the letters of the concerned word in the words
+// After that the thing that follows is touse the .join method to put the individual letters of the reversed word together
+// IF is used to check whether the word is a palindrome by checking whether the reversed word is the same as the initial word
 
 /*
   Add written explanation of your solution here
@@ -20,6 +31,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("")
+
+  console.log("Expecting: false")
+  console.log("=>", isPalindrome("ab"))
+
+  console.log("")
+
+  console.log("Expecting: true")
+  console.log("=>", isPalindrome("abba"))
 }
 
 module.exports = isPalindrome;
